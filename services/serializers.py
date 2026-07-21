4027d08c5539f5c15f8d6677049958359382b166
ArizonaKiller <sobeninnikita@yandex.ru>
2026-07-21 13:59:40 +0300

    Создана первая версия serializers.py.

---
+++ services/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from .models import User, Category, Service, Booking
+from django.utils import timezone
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'is_provider', 'phone_number']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'slug']
+
+class ServiceSerializer(serializers.ModelSerializer):
+    # Чтобы в API мы видели имя категории, а не ID
+    category_name = serializers.ReadOnlyField(source='category.name')
+    # Делаем provider только для чтения, будем назначать его в view.py автоматически
+    provider = serializers.ReadOnlyField(source='provider.username')
+
+    class Meta:
+        model = Service
+        fields = ['id', 'name', 'description', 'price', 'provider', 'category', 'category_name']
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Цена должна быть больше нуля.")
+        return value
+
+class BookingSerializer(serializers.ModelSerializer):
+    client = serializers.ReadOnlyField(source='client.username')
+
+    class Meta:
+        model = Booking
+        fields = ['id', 'client', 'service', 'booking_date', 'status', 'created_at']
+
+    def validate_booking_date(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError("Нельзя забронировать на прошлое время.")
+        return value
