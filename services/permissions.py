f09e8e91b53880c224e91d75734d972b6e9f9d1f
ArizonaKiller <sobeninnikita@yandex.ru>
2026-07-22 13:34:34 +0300

    Создана первая версия permissions.py. Обновлен views.py.

---
+++ services/permissions.py
@@ -0,0 +1,24 @@
+from rest_framework import permissions
+
+class IsProviderOrReadOnly(permissions.BasePermission):
+    """
+    Разрешает создание только пользователям с флагом is_provider.
+    Редактирование — только владельцу (provider).
+    Просмотр — всем (SAFE_METHODS).
+    """
+
+    def has_permission(self, request, view):
+        # Читать (GET, HEAD, OPTIONS) могут все
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Создавать (POST) может только провайдер и только авторизованный
+        return request.user.is_authenticated and request.user.is_provider
+
+    def has_object_permission(self, request, view, obj):
+        # SAFE_METHODS — это GET, HEAD, OPTIONS. Их разрешаем всем.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        # Редактировать или удалять может только сам автор услуги
+        # В твоей модели поле называется 'provider'
+        return obj.provider == request.user
--- services/views.py
+++ services/views.py
@@ -1,12 +1,12 @@
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
 from .models import Service, Category, Booking, User
 from .serializers import ServiceSerializer, CategorySerializer, BookingSerializer, UserSerializer
+from .permissions import IsProviderOrReadOnly
 
 class ServiceViewSet(viewsets.ModelViewSet):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
-    # permission_classes = [AllowAny]
+    permission_classes = [IsProviderOrReadOnly]
 
     def perform_create(self, serializer):
         # Это магия DRF: когда мы сохраняем новую услугу, 
@@ -16,12 +16,10 @@ class ServiceViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    # permission_classes = [AllowAny]
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-    # permission_classes = [AllowAny]
 
     def perform_create(self, serializer):
         # Аналогично для бронирования: текущий юзер = клиент
@@ -30,4 +28,3 @@ class BookingViewSet(viewsets.ModelViewSet):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    # permission_classes = [AllowAny]
